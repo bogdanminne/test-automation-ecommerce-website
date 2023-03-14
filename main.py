51f0f5923b2695bca4b017d61429fd8b2c2d742f
bogdanminne <bogdannsdap@gmail.com>
2023-03-14 10:06:11 +0200

    Test case 1

--- main.py
+++ main.py
@@ -9,6 +9,18 @@ website = 'http://127.0.0.1:8000/'
 driver = webdriver.Chrome()
 driver.get(website)
 
+#Test case 1 - Check customer login with invalid data
+signin = driver.find_element(By.XPATH,'//a[contains(@href,"account/signin")]')
+signin.click()
+username = driver.find_element(By.NAME, 'username')
+username.send_keys('testing2')
+password = driver.find_element(By.NAME, 'password')
+password.send_keys('testinginvalid')
+signin_button = driver.find_element(By.CSS_SELECTOR, 'body > div > div > div.col-12.col-sm-12.col-md-12.col-lg-6.bg-light > div > form > button')
+signin_button.click()
+driver.save_screenshot('Testcase1.jpg')
+
+#Test case 2 - 
 
 
 
